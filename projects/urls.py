07ff950eab02980901e93d6fc977018a9d52b129
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-10-13 15:14:29 -0300

    Requisito 4 teste2

--- projects/urls.py
+++ projects/urls.py
@@ -14,7 +14,7 @@ router.register(r'profiles', ProfileViewSet)
 urlpatterns = [
     path('profiles/', ProfileList.as_view(), name='profile-list'),
     path('profiles/<int:pk>/', ProfileDetail.as_view(), name='profile-detail'),
-    path('projects/', ProjectList.as_view(), name='project-list', ProfileViewSet),
-    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail', ProfileViewSet),
+    path('projects/', ProjectList.as_view(), name='project-list'),
+    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
     path('', include(router.urls)),
 ]
